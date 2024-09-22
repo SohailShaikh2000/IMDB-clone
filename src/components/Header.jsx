3ff4004ddf33ed91a002c1fe019a717ce504072b
Sohail <sohailliyakatshaikh@gmail.com>
2024-09-22 13:48:03 +0530

    add header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link';
+import MenuItem from './MenuItem'
+import { FaHome } from "react-icons/fa";
+import { BsFillInfoCircleFill } from "react-icons/bs"
+
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+      <div className='flex gap-4'>
+        <MenuItem title="home" address="/" Icon={FaHome} />
+        <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <Link href="/" className="flex gap-1 items-center">
+    <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDB</span>
+    <span className='text-xl hidden sm:inline'>Clone</span>
+      </Link>
+    </div>
+  )
+}
+
+export default Header
