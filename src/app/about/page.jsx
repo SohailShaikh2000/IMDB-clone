70f1b520b0252ae1ef6430e1b79efbb1bcf0c465
Sohail <sohailliyakatshaikh@gmail.com>
2024-09-22 14:46:08 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='max-w-6x; mx-auto p-3 gap-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias maxime rerum ut dicta quae molestias voluptatum? Accusamus beatae, recusandae nam consequuntur accusantium, dolorem obcaecati quaerat doloremque architecto, soluta consequatur dolor? Nobis quia ipsa quasi in aperiam enim, rerum ea omnis necessitatibus expedita voluptatibus ab temporibus ullam ducimus dolorum, recusandae cum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae dolore nesciunt ab suscipit quidem dolores ea exercitationem aut reiciendis cum libero, earum labore commodi est ex officia modi, unde nisi doloribus! Doloribus consequatur exercitationem quo harum minima assumenda itaque ullam nesciunt quae aliquid ea, dicta, distinctio fuga ipsa maiores. Repellat.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae voluptate iusto aperiam distinctio facere animi ratione eius laborum aut odit, dolorum est sunt ea excepturi possimus laboriosam dolore magnam sint maxime eligendi ipsum! Nostrum in dignissimos quibusdam suscipit veniam sunt, veritatis repellendus delectus? Rerum perspiciatis voluptatem molestiae enim labore delectus!</p>
+    </div>
+  )
+}
+
+export default About
